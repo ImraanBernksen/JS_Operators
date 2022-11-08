168024022eaec0ecdc97a476f07f899f7a4c2ae1
ImraanBernksen <imraanbernksen@gmail.com>
2022-11-08 13:39:48 +0200

    first commit

---
+++ js/code.js
@@ -0,0 +1,37 @@
+// Addition
+function addition() {
+    console.log(2 + 2);
+}
+// Subtraction
+function subtraction() {
+    console.log(5 - 2);
+}
+//Multiplication
+function multiplication() {
+    console.log(5 * 2);
+}
+//Division
+function division() {
+    console.log(5 / 2);
+}
+//Exponent
+function exponent() {
+    console.log(2 ** 2);
+}
+//Modulus
+function modulus() {
+    console.log(5 % 2);
+}
+// Call all functions
+// addition();
+// subtraction();
+// multiplication();
+// division();
+// exponent();
+// modulus();
+
+// Assignment Operators
+//let numb1 = 3;
+// numb = numb1 + 2;
+//result /= 2;
+//console.log(numb1);
